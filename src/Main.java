89446e5f66f23d4d31b69d1e3ec21eb7fb44e474
Nehaasati <31asatineha@gmail.com>
2025-02-11 11:40:48 +0100

    FIX error now everything working

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.sql.*;
+import java.util.*;
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+                System.out.println("\n=== MinBank System ===");
+                System.out.println("1. Login");
+                System.out.println("2. Exit");
+                System.out.print("Choose an option: ");
+
+                int choice = scanner.nextInt();
+                scanner.nextLine();
+                switch (choice) {
+                    case 1 -> Login.loginUser(scanner);
+                    case 2 -> Login.loginAdmin(scanner);
+                    case 3 -> {
+                        System.out.println("Exiting...");
+                        return;
+                    }
+                    default -> System.out.println("Invalid choice! Try again.");
+                }
+            }
+        }
     }
-}
